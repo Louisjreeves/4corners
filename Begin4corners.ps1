79d696f466591884b6ad1d5dbcb8d57e650d7f73
Louisjreeves <79279019+Louisjreeves@users.noreply.github.com>
2023-07-26 15:50:32 -0500

    Add files via upload

---
+++ Begin4corners.ps1
@@ -0,0 +1,27 @@
+﻿ 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+ 
+$mydownloads = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+
+$MyTemp =(Get-Item $mydownloads).fullname
+ set-location -Path $mytemp
+ 
+$myloca = "$mytemp\"
+ 
+ try
+ {
+
+$response = Invoke-WebRequest -Uri https://github.com/Louisjreeves/MiscRepair/raw/main/CornersTestandGraph.zip -OutFile $MyTemp\CornersTestandGraph.zip  
+ } catch 
+ {
+    $StatusCode = $_.Exception.Response.StatusCode.value__
+  }
+  
+
+      Expand-Archive -Path $mydownloads\CornersTestandGraph.zip -DestinationPath $mydownloads\CornersTestandGraph\ -Force
+ 
+ 
+ 
+ 
+ set-location "$mydownloads\CornersTestandGraph\" 
+  .\CornersTestandGraph.ps1
